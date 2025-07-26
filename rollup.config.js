fb7d4b47beded1ace12a3c383bd649fe05a7bda7
Soin Andrey <andrey.soin.work@gmail.com>
2025-07-26 22:48:31 +0300

    add new modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
-// rollup.config.js
+import copy from 'rollup-plugin-copy';
+
 export default {
     input: 'index.js',
     output: {
         file: 'dist/index.js',
         format: 'esm',
         sourcemap: true
-    }
-};
+    },
+    plugins: [
+        copy({
+            targets: [
+                { src: 'src/**/*.{css,scss,md,js,html}', dest: 'dist/src' }
+            ],
+            flatten: false // сохраняет структуру папок
+        })
+    ]
+}
